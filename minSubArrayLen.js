30db45fbe75d6a0cd4627ba2010ffc05ce47488b
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-02-20 18:28:22 -0800

    power

---
+++ minSubArrayLen.js
@@ -0,0 +1,7 @@
+//write a function accepts an array of positive integers, and a positive value
+//function should return the minimal length of a contiguous subarray of which the sum
+//is greater than or equal to the integer passed to the function.if there isn't one return zero
+
+function minSubArrayLen(array, n) {}
+
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7));
