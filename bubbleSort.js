6f4f0fd0441c32f62a7565cfb471892a25bdfe09
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-02-22 20:43:08 -0800

    bubblesort

---
+++ bubbleSort.js
@@ -0,0 +1,22 @@
+function bubbleSort(array) {
+  let noSwap;
+  for (let i = 0; i < array.length; i++) {
+    noSwap = true;
+    for (let j = 0; j < array.length - i; j++) {
+      console.log(array[i], array[j]);
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        noSwap = false;
+      }
+    }
+    console.log(array);
+    if (noSwap) {
+      console.log("break");
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([3, 2, 1, 8, 9, 10, 12, 14]));
